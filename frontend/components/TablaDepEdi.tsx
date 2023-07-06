42c52fc5b2db87762ead6bad53bfc6f41f2305ef
Jose Luis <65769995+jls-tuc@users.noreply.github.com>
2023-07-05 21:18:39 -0300

    Se corrigen los problemos almomento de actualizar y borrar una dependencia

--- frontend/components/TablaDepEdi.tsx
+++ frontend/components/TablaDepEdi.tsx
@@ -3,22 +3,45 @@ import { IDep } from "Frontend/interface/TsjApp.Interface";
 import React, { useState } from "react";
 
 import { ModalDependencia } from "./ModalDependencia";
+import { msgBorrar, msgOk } from "./MsgSwal";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 interface props {
   datos?: any;
   menu?: string;
   id_edificio: number;
 }
 export const TablaDepEdi: React.FC<props> = ({ datos, menu, id_edificio }) => {
+  let navigate = useNavigate();
   const [openModal, setOpenModal] = useState<boolean>(false);
   const [ediSelect, setEditSelec] = useState<any>({});
   const [titulo, setTitulo] = useState<string>("");
+  const [idEdificio, setIdEdificio] = useState<number>(0);
 
   const closeModal = () => {
     setOpenModal(!openModal);
   };
-  const borrarDependencia = (dependencia: IDep) => {
-    DependenciasEndPoint.deleteDependencia(dependencia).then((res) => {
-      console.log(res);
+  const borrarDependencia = async (dependencia: IDep) => {
+    await msgBorrar().then((result: any) => {
+      if (result.isConfirmed) {
+        let data = {
+          id: dependencia.id,
+          nombre: dependencia.nombre,
+          domicilio: dependencia.domicilio,
+        };
+
+        DependenciasEndPoint.deleteDependencia(data)
+          .then(async (res) => {
+            await msgOk();
+            navigate("/edificios");
+          })
+          .catch(async (err: any) => {
+            await msgOk();
+            navigate("/edificios");
+          });
+      } else {
+        Swal.fire("No se realizo ninguna modificación", "", "info");
+      }
     });
   };
 
@@ -30,7 +53,7 @@ export const TablaDepEdi: React.FC<props> = ({ datos, menu, id_edificio }) => {
             <span className="card-label fw-bolder fs-3 mb-1">{menu}</span>
           </h3>
 
-          {menu !== "ABM de Dependencias" && (
+          {menu !== "Dependencias Creadas" && (
             <div className="col-lg-4 fv-row">
               <button
                 className="btn btn-outline-primary mr-3"
@@ -55,7 +78,7 @@ export const TablaDepEdi: React.FC<props> = ({ datos, menu, id_edificio }) => {
             <tr>
               <th className="min-w-100px">Nombre</th>
               <th className="min-w-120px">Domicilio</th>
-              {menu === "ABM de Dependencias" && (
+              {menu === "Dependencias Creadas" && (
                 <th className="min-w-120px">Edificio al que pertenece</th>
               )}
               <th className="min-w-150px">Opciones</th>
@@ -68,7 +91,7 @@ export const TablaDepEdi: React.FC<props> = ({ datos, menu, id_edificio }) => {
                   <tr className="fw-bolder text-muted" key={element.id}>
                     <td>{element.nombre}</td>
                     <td>{element.domicilio}</td>
-                    {menu === "ABM de Dependencias" && (
+                    {menu === "Dependencias Creadas" && (
                       <td>{element.edificio_nombre}</td>
                     )}
 
@@ -78,6 +101,7 @@ export const TablaDepEdi: React.FC<props> = ({ datos, menu, id_edificio }) => {
                         onClick={() => {
                           setTitulo("Editar Dependencia");
                           setEditSelec(element);
+                          setIdEdificio(element.id_edificio);
                           setOpenModal(true);
                         }}
                       >
@@ -112,7 +136,7 @@ export const TablaDepEdi: React.FC<props> = ({ datos, menu, id_edificio }) => {
           closeModal={closeModal}
           dependencia={ediSelect}
           titulo={titulo}
-          id_edificio={id_edificio}
+          id_edificio={id_edificio || idEdificio}
         />
       </div>
     </>
